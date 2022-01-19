4836be2c68c39f28420c1452a630d4d96e7a6cad
kvlasic <kristijan.vlasic1@gmail.com>
2022-01-19 21:27:34 +0100

    Co-authored-by: Johannes Geier <Timbuktujoe@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { Map, Marker } from "pigeon-maps";
 
 function App() {
+  const [ipInfo, setIpInfo] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((response) => setIpInfo(response));
+  }, []);
+
+  const latitude = ipInfo?.location?.lat;
+  const longitude = ipInfo?.location?.lng;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <p>Your IPv4 Address is: {ipInfo.ip}</p>
+      <p>
+        Location: {ipInfo?.location?.city}, {ipInfo?.location?.region},{" "}
+        {ipInfo?.location?.country}
+      </p>
+      <p>ISP: {ipInfo.isp}</p>
+      {latitude && (
+        <Map
+          height={600}
+          defaultCenter={[latitude, longitude]}
+          defaultZoom={11}
         >
-          Learn React
-        </a>
-      </header>
+          <Marker width={50} anchor={[latitude, longitude]} />
+        </Map>
+      )}
     </div>
   );
 }
+
+// ipInfo.location.lat & ipInfo.location.lng => https://pigeon-maps.js.org/docs/installation
 
 export default App;
