dc669679d05c6e4aa31bbf376a6105dac29f6d7f
kvlasic <kristijan.vlasic1@gmail.com>
2022-01-20 15:29:04 +0100

    added some bad looking styling, fix or change

--- src/App.js
+++ src/App.js
@@ -9,10 +9,34 @@ function App() {
   // fetch information about IP-Address and Location
   useEffect(() => {
     fetch(
-      `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${process.env.REACT_APP_API_KEY}`
+      `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${process.env.REACT_APP_API_KEY}&ipAddress=8.8.8.8`
     )
       .then((response) => response.json())
-      .then((response) => setIp(response));
+      .then((response) => setIp(response))
+      .then(
+        fetch(`https://countries.trevorblades.com/`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            query: `
+            query {
+            country(code: "${ip?.location?.country}"){
+                name
+                native
+                continent {name}
+                capital
+                currency
+                languages {
+                  name
+                }
+              }
+            }
+            `,
+          }),
+        })
+          .then((response) => response.json())
+          .then((response) => setCountry(response.data.country))
+      );
   }, []);
 
   // save coordinates in variables
@@ -20,54 +44,39 @@ function App() {
   const longitude = ip?.location?.lng;
 
   // fetch country information
-  useEffect(() => {
-    fetch(`https://countries.trevorblades.com/`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        query: `
-          query {
-            country(code: "DE"){
-              name
-              native
-              continent {name}
-              capital
-              currency
-              languages {
-                name
-              }
-            }
-          }
-          `,
-      }),
-    })
-      .then((response) => response.json())
-      .then((response) => setCountry(response.data.country));
-  }, []);
+  // useEffect(() => {
+  //   ;
+  // }, []);
 
   console.log(country);
 
   // render
   return (
     <div className="App">
-      <div>
-        <h1>IP information</h1>
-        <p>Your IPv4 Address is: {ip.ip}</p>
-        <p>
-          Location: {ip?.location?.city}, {ip?.location?.region},{" "}
-          {ip?.location?.country}
-        </p>
-        <p>ISP: {ip.isp}</p>
+      <div className="container">
+        <div className="glass">
+          <h1>IP information</h1>
+          <div className="info">
+            <p>Your IPv4 Address is: {ip?.ip}</p>
+            <p>
+              Location: {ip?.location?.city}, {ip?.location?.region},{" "}
+              {ip?.location?.country}
+            </p>
+            <p>ISP: {ip?.isp}</p>
+          </div>
+        </div>
+        <div className="glass">
+          <h1>Country information</h1>
+          <div className="info">
+            <p>Continent: {country?.continent?.name}</p>
+            <p>Name: {country?.name}</p>
+            <p>Native name: {country?.native}</p>
+            <p>Capital: {country?.capital}</p>
+            <p>Currency: {country?.currency}</p>
+          </div>
+        </div>
       </div>
-      <div>
-        <h1>Country information</h1>
-        <p>Continent: {country?.continent?.name}</p>
-        <p>Name: {country?.name}</p>
-        <p>Native name: {country?.native}</p>
-        <p>Capital: {country?.capital}</p>
-        <p>Currency: {country?.currency}</p>
-        {/* <p>Languages: {country?.languages[0]?.name}</p> */}
-      </div>
+
       {latitude && (
         <Map
           height={600}
